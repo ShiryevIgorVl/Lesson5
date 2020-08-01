5cb153315e257ba1a3bf70c9bb924aaae6af27e3
ShiryevIgorVl <ishirayev@gmail.com>
2020-08-01 11:53:05 +0300

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,19 @@
+public class Employee {
+
+    public String surnameNamePatronymic;
+    public String position;
+    public String email;
+    public int telephone;
+    public int salary;
+    public int age;
+
+    public void Employee (String surnameNamePatronymic, String position, String email, int telephone, int salary, int age) {
+       this.surnameNamePatronymic = surnameNamePatronymic;
+       this.position = position;
+       this.email = email;
+       this.telephone = telephone;
+       this.salary = salary;
+       this.age = age;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void person (String[] args) {
+        Employee Employee = new Employee();
+        // ("Иванов Иван Иванович", "нженер", "123@yandex.ru", 1234567, 30000, 40);
+        Employee = filler ("Иванов Иван Иванович", "нженер", "123@yandex.ru", 1234567, 30000, 40)
+
+    }
+}
