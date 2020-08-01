09412832f8ea500ea21e3cd8d77db17468df5979
ShiryevIgorVl <ishirayev@gmail.com>
2020-08-01 13:09:10 +0300

    ok

--- src/Employee.java
+++ src/Employee.java
@@ -7,7 +7,9 @@ public class Employee {
     public int salary;
     public int age;
 
-    public void Employee (String surnameNamePatronymic, String position, String email, int telephone, int salary, int age) {
+
+
+    public  Employee(String surnameNamePatronymic, String position, String email, int telephone, int salary, int age) {
        this.surnameNamePatronymic = surnameNamePatronymic;
        this.position = position;
        this.email = email;
@@ -15,5 +17,16 @@ public class Employee {
        this.salary = salary;
        this.age = age;
     }
+
+    public void  printEmp() {
+        System.out.printf("ФИО: %s; Должность: %s; Адрес электронной почты: %s; Телефон: %s; Зарплата, т.руб: %s; Возраст, лет: %s \n", surnameNamePatronymic, position, email, telephone, salary, age);
+    }
+
+    public static void main (String[] arge) {
+        Employee employee1 = new Employee("Иванов Иван Иванович", "нженер", "123@yandex.ru", 1234567, 30, 40);
+        employee1.printEmp();
+    }
+
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
 
-    public static void person (String[] args) {
-        Employee Employee = new Employee();
-        // ("Иванов Иван Иванович", "нженер", "123@yandex.ru", 1234567, 30000, 40);
-        Employee = filler ("Иванов Иван Иванович", "нженер", "123@yandex.ru", 1234567, 30000, 40)
 
+
+    public static void main (String[] arge) {
+        Employee[] persArray = new Employee[5];
+        persArray[0] = new Employee("Иванов Иван", "нженер", "123@yandex.ru", 1234567, 30, 40);
+        persArray[1] = new Employee("Петров Петр", "нженер 2 кат.", "1234@yandex.ru", 2234567, 32, 24);
+        persArray[2] = new Employee("Васильев Василий", "нженер 2 кат.", "12345@yandex.ru", 3234567, 33, 44);
+        persArray[3] = new Employee("Смирнов Евгений", "вед. нженер", "12345@yandex.ru", 4234567, 35, 35);
+        persArray[4] = new Employee("Матвеев Денис", "нженер 2 кат.", "12346@yandex.ru", 6234567, 28, 34);
+        for (int i = 0; i < persArray.length; i++) {
+            if (persArray[i].age >= 40) {
+                persArray[i].printEmp();
+            }
+        }
     }
 }
